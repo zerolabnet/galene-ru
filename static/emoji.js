f58ec33dd5b83bf8a59df6073dd7808a1c27a0f2
ZeroChaos <71016736+zerolabnet@users.noreply.github.com>
2023-07-28 17:10:47 +0300

    Additional emoji, popup panel

--- static/emoji.js
+++ static/emoji.js
@@ -3,6 +3,8 @@
 document.addEventListener('DOMContentLoaded', function () {
   const inputTextarea = document.getElementById('input');
   const emojiButtons = document.querySelectorAll('.emoji');
+  const additionalEmojiButton = document.getElementById("additionalEmojiButton");
+  const additionalEmojiPopup = document.getElementById("additionalEmojiPopup");
 
   function insertEmoji(event) {
     if (event.target.classList.contains('emoji')) {
@@ -21,6 +23,20 @@ document.addEventListener('DOMContentLoaded', function () {
       button.addEventListener('click', insertEmoji);
     });
   }
+
+  function hidePopupOnClickOutside(event) {
+    if (!additionalEmojiPopup.contains(event.target) && event.target !== additionalEmojiButton) {
+      additionalEmojiPopup.style.display = "none";
+    }
+  }
+
+  document.addEventListener('click', hidePopupOnClickOutside);
+
+  additionalEmojiButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    additionalEmojiPopup.style.display =
+      additionalEmojiPopup.style.display === "block" ? "none" : "block";
+  });
 
   setupEmojiButtons();
 });
